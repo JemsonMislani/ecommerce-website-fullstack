ecaf55062535548265fadabdc216b5f12f43f4d8
Jemson <jemsonmislani@gmail.com>
2026-07-02 12:31:12 +0800

    create an animation for pages.

--- ecom-frontend/src/components/Home.jsx
+++ ecom-frontend/src/components/Home.jsx
@@ -1,19 +1,49 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import HomeSlider from "./HomeSliderImages";
 import SectionPage from "./SectionPage";
 import CollectionPage from "./CollectionPage";
 import AboutUs from "./AboutUs";
 import FooterPage from "./FooterPage";
+import './Home.css'
 
 export default function Home(){
+        useEffect(() => {
+        const animations = document.querySelectorAll(".animation");
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry)=>{
+                    if(entry.isIntersecting){
+                        entry.target.classList.add("active");
+                    }
+                });
+            },
+            {
+                threshold:.2
+            }
+        );
+        animations.forEach((el)=>observer.observe(el));
+        return ()=>observer.disconnect();
+    },[]);
+
     return(
         <>
             <Header />
+        <div className="animation">
             <HomeSlider />
+        </div>
+        <div className="animation">
             <SectionPage />
+        </div>
+        <div className="animation">
             <CollectionPage />
+        </div>
+        <div className="animation">
             <AboutUs />
+        </div>
+        <div className="animation">
             <FooterPage />
+        </div>
         </>
     );
 }
