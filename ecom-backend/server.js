1b1b25bd808c8a9a1bb3cd927d9d131d96deb70e
Jemson <jemsonmislani@gmail.com>
2026-07-03 16:37:31 +0800

    create an api to insert data from product variants. Test to postman, 200 OK.

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -12,6 +12,10 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
 
+
+// -FOR PRODUCTS-
+
+// create product data
 app.post('/createProducts', async(req, res) => {
 
     try {
@@ -30,6 +34,19 @@ app.get('/getProductData', async(req, res) => {
     try {
         const result = await pool.query('SELECT * FROM products')
         res.json(result.rows)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Server Error')
+    }
+})
+
+// create product data variants
+app.post('/createProductDataVariants', async(req, res) => {
+
+    try {
+        const { prod_id, prod_size, prod_color, prod_img_hover, prod_desc_threelines } = req.body
+        const result = await pool.query('INSERT INTO product_variants (prod_id, prod_size, prod_color, prod_img_hover, prod_desc_threelines) VALUES ($1, $2, $3, $4, $5 ) RETURNING *', [ prod_id, prod_size, prod_color, prod_img_hover, prod_desc_threelines ])
+        res.json(result.rows[0])
     } catch (error) {
         console.log(error)
         res.status(500).send('Server Error')
