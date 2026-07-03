e006575123fdfc885a1f16e881909f172d54d834
Jemson <jemsonmislani@gmail.com>
2026-07-03 14:41:45 +0800

    create an api to insert/create a product data to database.

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -12,6 +12,18 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
 
+app.post('/createProducts', async(req, res) => {
+
+    try {
+        const { prod_img, prod_name, prod_price, prod_desc } = req.body
+        const result = await pool.query('INSERT INTO products (prod_img, prod_name, prod_price, prod_desc) VALUES ($1, $2, $3, $4) RETURNING *', [ prod_img, prod_name, prod_price, prod_desc ])
+        res.json(result.rows[0])
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Server Error')
+    }
+})
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Jem your server is running on port ${PORT}`)
