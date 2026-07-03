53c872d5462f420135bca079990d99a4ef63f5bf
Jemson <jemsonmislani@gmail.com>
2026-07-03 14:21:49 +0800

    Install packages and dependencies. connect server to database postgres.

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const { Pool } = require('pg')
+require('dotenv').config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
